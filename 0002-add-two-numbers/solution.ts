76a2978dc941df63ae57ab77d6893e3a87617a19
MY-NAME-IS-IVAN <hereisivannn@gmail.com>
2025-05-21 20:40:59 -0700

    2

---
+++ 0002-add-two-numbers/solution.ts
@@ -0,0 +1,50 @@
+// Description: You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+// Link: https://leetcode.com/problems/add-two-numbers/description/
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  const dummy = new ListNode(0);
+  let current = dummy;
+  let carry: number = 0;
+
+  while (l1 || l2 || carry) {
+    const sum: number = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+    current.next = new ListNode(sum % 10);
+    carry = Math.floor(sum / 10);
+
+    current = current.next;
+
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+
+  return dummy.next;
+}
